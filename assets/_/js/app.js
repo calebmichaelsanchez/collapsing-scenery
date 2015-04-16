19e15b3b70dbafd7b366fac64cdd043fb865f7b6
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2015-04-16 04:47:19 -0400

    Get proper tweet url

--- assets/_/js/app.js
+++ assets/_/js/app.js
@@ -193,7 +193,7 @@ var twitterInit = function(){
 	$('.twitter-feed').twittie({
 	    username: 'collapsingscene',
 	    dateFormat: '%m/%d/%y',
-	    template: '<div class="tweet-container"><div class="date">{{date}}</div><div class="tweet">{{tweet}}... <a class="read-more" href="{{link}}">Read More</a></div></div>',
+	    template: '<div class="tweet-container"><div class="date">{{date}}</div><div class="tweet">{{tweet}}... <a class="read-more" href="{{url}}">Read More</a></div></div>',
 	    count: 3
 	});
 }
